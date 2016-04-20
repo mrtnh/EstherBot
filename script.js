1b6b6f17ac5269f8b981ea49c5bc1d1b08a24bb7
mrtnh <martinhoffmann@outlook.com>
2016-04-20 18:13:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Du willst mehr über Martin Hoffmann wissen? Dann schreib einfach HELLO, um anzufangen!')
                 .then(() => 'speak');
         }
     },
